11195a53aec7c2af578e1bc1c070987a480a5e1e
Dei <62786780+jeongde2@users.noreply.github.com>
2022-06-10 22:19:56 +0900

    [Camera] Introduce a Camera View

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="home">
-    홈화면(d2)
+    <video id="video" autoplay playsinline></video>
+    <input type="file" accept="image/*" capture="camera" />
+    <button>촬영</button>
   </div>
 </template>
 
 <script>
+const userMedia = navigator.mediaDevices.getUserMedia({
+  audio: false,
+  video: {
+    width: 640,
+    height: 480,
+    facinMode: 'environment'
+  }
+})
+userMedia.then(function (stream) {
+  const videoEl = document.getElementById('video')
+  const videoTracks = stream.getVideoTracks()[0]
+  console.log('cccc : ' + videoTracks.label)
+  videoEl.srcObject = stream
+}).catch(function (error) {
+  console.error(error.message)
+})
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+  },
+  data () {
+    return {
+      showCamera: false
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
