990a14e69bb94f0de8a7c50643c0634ecf6ca94f
joy103 <78711544+joy103@users.noreply.github.com>
2022-06-18 08:27:53 +0900

    delete firebase config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,17 +11,6 @@ import './styles/common.scss'
 
 Vue.config.productionTip = false
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCTeWMiV4R17mqHC-2fNSu10-vm4ITfe8k',
-  authDomain: 'miraclemorning-328be.firebaseapp.com',
-  databaseURL: 'https://miraclemorning-328be-default-rtdb.firebaseio.com',
-  projectId: 'miraclemorning-328be',
-  storageBucket: 'miraclemorning-328be.appspot.com',
-  messagingSenderId: '993767488143',
-  appId: '1:993767488143:web:35a0fcc6a7f9e0673f8efa',
-  measurementId: 'G-T8RF2RR71K'
-}
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
