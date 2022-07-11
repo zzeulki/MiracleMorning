2aaf7c42897e4a4cfede5876699ff58b9b77165d
zzeulki <seul0411@naver.com>
2022-07-11 20:17:28 +0900

    account UI changed

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -3,11 +3,11 @@
     <h1 style="font-weight:700" >안녕하세요?</h1>
     <h1 class=name_lavel>당신의 이름은</h1>
     <div class=name_input>
-      <input id="nickName" style="height:6vh; border:3px solid #000000; text-align:center; border-radius: 10px;" type="text" placeholder="미라클모닝">
+      <input id="nickName" type="text" placeholder="미라클모닝">
     </div>
     <h1 class=name_lavel_2> 입니다.</h1>
-    <v-btn class=next_btn @click="movePage('InitSetUp')">
-      <v-icon>mdi-arrow-right-bold-circle</v-icon>
+    <v-btn depressed class=next_btn @click="movePage('InitSetUp')">
+      <v-icon class=arrow-btn>mdi-arrow-right</v-icon>
     </v-btn>
   </div>
 </template>
@@ -28,7 +28,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@/styles/_color.scss';
 .init_account{
   margin-top:20vh;
   padding:10%;
@@ -55,5 +56,18 @@ export default {
 .next_btn{
   margin-left : 68vw;
   margin-top : 2.2vh;
+}
+.theme--light.v-btn {
+    color: gray;
+}
+.theme--light.v-btn.v-btn--has-bg
+{
+  background-color:$key-color-blue !important;
+}
+.name_input>input {
+    height: 6vh;
+    border: 3px solid $key-color-blue;
+    text-align: center;
+    border-radius: 10px;
 }
 </style>
